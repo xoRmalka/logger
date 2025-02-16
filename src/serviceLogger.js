5ebe3ef9a4ed5c51b47df7548dd5cadbc3da3cad
Or Malka <or.malka@taboola.com>
2025-02-16 18:31:41 +0200

    feat: ServiceLogger mock lo

--- src/serviceLogger.js
+++ src/serviceLogger.js
@@ -1,29 +1,29 @@
 // 3rd Party Logging Service API
 const ServiceLogger = {
     init(apiKey, apiEndpoint) {
-        // Configure the logging service with an API key
+        // Configure the logging service with an API key and endpoint
         console.log("Initializing logging service with API key:", apiKey);
         console.log("Initializing logging service with API endpoint:", apiEndpoint);
     },
 
     log(...args) {
-        // Send standard log message
-        console.log("LOG - Logging message to 3rd party service:", ...args);
+        // Send standard log message 
+        console.log("LOG message to 3rd party service:", ...args);
     },
 
     info(...args) {
         // Send informational log message
-        console.info("INFO - Logging message to 3rd party service:", ...args);
+        console.info("INFO message to 3rd party service:", ...args);
     },
 
     warn(...args) {
         // Send warning log message
-        console.warn("WARN - Logging message to 3rd party service:", ...args);
+        console.warn("WARN message to 3rd party service:", ...args);
     },
 
     error(...args) {
         // Send error log message
-        console.error("ERROR - Logging message to 3rd party service:", ...args);
+        console.error("ERROR message to 3rd party service:", ...args);
     },
 };
 
